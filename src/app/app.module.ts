76c7a9d181e5753593d1743b4207420a9b769158
Artem Karpukhin <a.karpukhin9@gmail.com>
2023-03-21 08:59:30 +0200

    feat: add lazy loading (16.20)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AuthModule } from './auth/auth.module';
-import { TodosModule } from './todos/todos.module';
 import { CoreModule } from './core/core.module';
 import { HttpClientModule } from '@angular/common/http';
 import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    AuthModule,
-    TodosModule,
-    CoreModule,
-    HttpClientModule,
-    SharedModule,
-  ],
+  imports: [BrowserModule, AppRoutingModule, CoreModule, HttpClientModule, SharedModule],
   providers: [],
   bootstrap: [AppComponent],
 })
